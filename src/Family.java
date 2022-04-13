048865404a5419d14a22e6e35fe4f9d82f120070
Siddharth Thakur <insthak@KMC.Corp>
2022-04-13 16:48:26 +0530

    first commit

---
+++ src/Family.java
@@ -0,0 +1,5 @@
+public class Family {
+    public static void main(String args[]){
+
+        System.out.print("Thakur");
+    }}
---
+++ src/FirstCode.java
@@ -0,0 +1,15 @@
+public class FirstCode {
+    public static void main(String args[]) {
+
+        System.out.println("Hello World");
+        int i=5;
+        System.out.println(i);
+ int j=15;
+ int k=15;
+
+ System.out.println(j);
+ int num=5;
+ System.out.println(num);
+
+    }}
+
